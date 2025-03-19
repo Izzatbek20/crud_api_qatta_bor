e433d50574820ee82d7e7b5dcb18939dafda72ec
Ravshanbek <rasidsobirov38@gmail.com>
2025-03-19 13:40:07 +0500

    api.py da o'zgartirishlar

--- app/api.py
+++ app/api.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 # Auth
 from .auth.login import auth_route
 
-from app.routes import user, article_banner, article_media, article, article_saved, article_view, category
+from app.routes import user, article_banner, article_media, article, article_saved, article_view, category, region, tg_user, settings
 
 api = APIRouter()
 
@@ -18,3 +18,6 @@ api.include_router(article_media.router, tags=["Article media"])
 api.include_router(article_saved.router, tags=["Article saved"])
 api.include_router(article_view.router, tags=["Article view"])
 api.include_router(category.router, tags=["Category"])
+api.include_router(region.router, tags=["Region"])
+api.include_router(tg_user.router, tags=["Telegram user"])
+api.include_router(settings.router, tags=["Settings"])
