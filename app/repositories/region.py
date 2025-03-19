057cffc2acdba8d2f42497df21e5a0091ff2e5b3
unknown <umidjonaskaraliev@gmail.com>
2025-03-19 12:09:24 +0100

    Region apida o'zgarishlar

--- app/repositories/region.py
+++ app/repositories/region.py
@@ -3,43 +3,47 @@ from sqlalchemy.future import select
 
 from app.core.base import BaseRepository
 from app.models.region import Region
+from app.schemas.region import RegionStatus
 from app.utils.pagination import PageParams, pagination
 
 class RegionRepository(BaseRepository):
-    async def get_all(self, page_params: PageParams = None):
+    async def get_all(self, status: RegionStatus, parent_id: int, page_params: PageParams = None):
         """
             Barcha Regionlarni olish
         """
-        query = select(Region)
+        query = select(Region).where(Region.parent_id==0).where(Region.status==status)
+
+        if parent_id != 0 and status:
+            query = select(Region).where(Region.parent_id==parent_id).where(Region.status==status)
 
         if page_params:
             return await pagination(self.session, query, page_params)
         else:
             result = await self.session.execute(query)
             return result.scalars().all()
-        
+
     async def get_one(self, id: int):
         """
             Region haqida to'liq ma'lumot olish
         """
         query = select(Region).where(Region.id==id)
         result = await self.session.execute(query)
-        return result.scalar_one_or_none()
-    
+        return result.scalar()
+
     async def create(self, payload: dict, flush: bool = False, commit: bool = True) -> int | bool:
         """
             Yangi region yaratish
         """
         query = insert(Region).values(payload)
         exc = await self.session.execute(query)
-    
+
         if flush:
             await self.session.flush()
             return exc.inserted_primary_key[0]  # Flush qilinganda qo'shilgan ma'lumot id sini qaytaradi
         elif commit:
             await self.session.commit()
             return True
-    
+
     async def update(self, id: int, payload: dict, flush: bool = False, commit: bool = True) -> int | bool:
         """
             Region ma'lumotlarini yangilash
@@ -53,11 +57,12 @@ class RegionRepository(BaseRepository):
         elif commit:
             await self.session.commit()
             return True
-        
+
     async def delete(self, id: int):
         """
             Region o'chirish
         """
-        query = delete(Region).where(Region.id==id)
+        value = {"status": RegionStatus.DELETE}
+        query = delete(Region).where(Region.id==id).values(value)
         await self.session.execute(query)
         await self.session.commit()
--- app/schemas/region.py
+++ app/schemas/region.py
@@ -2,11 +2,21 @@ from pydantic import BaseModel
 from datetime import datetime
 from enum import Enum
 
-class RegionResponse(BaseModel):
-    id: int
+class RegionUpdatePayload(BaseModel):
+    parent_id: int
+    title: str | None
+    latitude: float | None
+    longitude: float | None
+
+class RegionCreatePayload(BaseModel):
+    parent_id: int
     title: str
     latitude: float
     longitude: float
+
+class RegionResponse(RegionCreatePayload):
+    id: int
+    status: "RegionStatus"
     created_at: datetime
     updated_at: datetime
 
@@ -14,6 +24,3 @@ class RegionStatus(str, Enum):
     ACTIV = "actived"
     INACTIV = "inactived"
     DELETE = "deleted"
-
-class RegionPayload(RegionResponse):
-    pass
